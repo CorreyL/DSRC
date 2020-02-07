02653dd1370e12d3fba2881e8cbc8e2cb77b1716
CorreyL <correylim@gmail.com>
2020-02-07 09:49:29 -0800

    Add class `DsrcInMemory`, private vars and fn sigs
    - Give rationale for existence of `DsrcInMemory` that does not exist in
      current implementation of `DSRC` codebase
    - Instantiate null pointers for `DsrcFileReader`, `DsrcDataChunk` and
      `FastqDataChunk` that will be assigned in the constructor
    - Add function signatures for constructor, destructor and function
      `getNextChunk` that will iterate through the opened dsrc file

--- include/dsrc/DsrcInMemory.h
+++ include/dsrc/DsrcInMemory.h
@@ -1,10 +1,37 @@
 #ifndef H_DSRCINMEMORY
 #define H_DSRCINMEMORY
 
+#include "../../src/DsrcFile.h"
+#include "../../src/DsrcIo.h"
+#include "../../src/Fastq.h"
+
 namespace dsrc{
   namespace comp {
     using namespace core;
     using namespace fq;
+
+    /**
+     * Utilizes the implementation of DsrcFileReader to read the binary contents
+     * of a .dsrc file into memory and return the uncompressed FASTQ content as
+     * a string
+     *
+     * This allows the contents of a .dsrc file to be uncompressed without
+     * needing to write the contents to a file, which can be beneficial when
+     * concatenating multiple .dsrc files into a single concatenated .fastq file
+     * with limited hard disk space (i.e. no longer needing to uncompress each
+     * .dsrc file into temporary .fastq files before concatenation)
+     */
+    class DsrcInMemory {
+      public:
+        DsrcInMemory(const std::string& dsrcFilename_);
+        ~DsrcInMemory();
+        std::string getNextChunk();
+
+      private:
+        DsrcFileReader* reader = NULL;
+        DsrcDataChunk* dsrcChunk = NULL;
+        FastqDataChunk* fastqChunk = NULL;
+    };
   }
 }
 
