4a4508b8fd397fd5c15fc2b0913afd74aa0d5eb4
CorreyL <correylim@gmail.com>
2020-02-07 09:56:31 -0800

    Implement DsrcInMemory constructor & destructor
    - Utilize the `DsrcFileReader` class to handle the decompression logic
    - Instantiate `DsrcDataChunk` to hold a chunk of the binary contents
      read from the input file
    - Instantiate `FastqDataChunk` to hold the decompressed content from
      `DsrcDataChunk`
    - Catch any thrown instances of `DsrcException` with a `@todo` comment
      to implement better error handling in the future (stop execution of
      the program with a non-zero exit should a `DsrcException` ever be
      caught)
    - In the destructor, reset the state of `dsrcChunk` and `fastqChunk`,
      close the `DsrcFileReader`, and delete the objects, ensuring all
      allocated memory is freed

--- src/DsrcInMemory.cpp
+++ src/DsrcInMemory.cpp
@@ -1,8 +1,52 @@
 #include "../include/dsrc/DsrcInMemory.h"
 
+#include "DsrcFile.h"
+#include "DsrcIo.h"
+#include "Buffer.h"
+#include "Fastq.h"
+
+#include <iostream>
+
 namespace dsrc{
   namespace comp {
     using namespace core;
     using namespace fq;
+
+    /**
+     * Creates a pointer to the given .dsrc file to read the buffer contents
+     * in chunks
+     *
+     * Allocates memory for dsrcChunk* for the buffer to be loaded into memory,
+     * allocates memory for fastqChunk* for the decompressed buffer contents to
+     * be loaded into memory
+     */
+    DsrcInMemory::DsrcInMemory(const std::string& dsrcFilename_) {
+      try {
+        reader = new DsrcFileReader();
+        reader->StartDecompress(dsrcFilename_);
+        dsrcChunk = new DsrcDataChunk(DsrcDataChunk::DefaultBufferSize);
+        fastqChunk = new FastqDataChunk(FastqDataChunk::DefaultBufferSize);
+      } catch (const DsrcException& e_) {
+        /**
+         * @todo Implement IsError() functionality? Extend from IDsrcOperator?
+         */
+        std::cerr << e_.what() << std::endl;
+        exit(1);
+      }
+    }
+
+    /**
+     * Removes all contents from memory allocated for dsrcChunk and fastqChunk,
+     * closes the decompression fileStream, and frees up all memory allocated
+     * for each pointer
+     */
+    DsrcInMemory::~DsrcInMemory() {
+      dsrcChunk->Reset();
+      fastqChunk->Reset();
+      reader->FinishDecompress();
+      delete dsrcChunk;
+      delete fastqChunk;
+      delete reader;
+    }
   }
 }
